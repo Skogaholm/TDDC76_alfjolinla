b2bc648038257798872bcf4881441e51dd088590
Alfred <alfjo519@student.liu.se>
2020-09-13 13:44:06 +0200

    Kompletering gjord
    Har kompleterat koden utifrån de givna kommentarerna

--- Lab0/uppgift1_kompletering.cc
+++ Lab0/uppgift1_kompletering.cc
@@ -1,23 +1,28 @@
 //####
 //2-11 Inkludera samtliga kodbibliotek som används i filen.
+//#svar# lagt till #include <string>
 #include <iostream>
 #include <iomanip>
+#include <string>
 using namespace std;
+
+//mainfunktion
 int main()
 {
-        //####
-        //2-2 - Engelska i kod.
+    //####
+    //2-2 - Engelska i kod.
+    //#svar# Ändrat variabelnamn
   	//Deklarera variabler
-  	int tal{0};
+  	int integer{0};
   	string text{" "};
-  	char tecken{' '};
-  	float flyttal{0.0};
+  	char character{' '};
+  	float floatnumber{0.0};
 
   	//Första delen
   	cout << "Skriv in ett heltal: " << flush;
-  	cin >> tal;
+  	cin >> integer;
   	cout << "Du skrev in heltalet: ";
-  	cout << tal << '\n' << endl;
+  	cout << integer << '\n' << endl;
 
   	//Nollställ cin, görs mellan alla delar
   	cin.clear();
@@ -25,58 +30,59 @@ int main()
 
   	//Andra delen
   	cout << "Skriv in fem  heltal: " << flush;
-  	cin >> tal;
+  	cin >> integer;
    	cout << "Du skrev in heltalen: ";
 	//#### 1-7 Undvik onödiga tomrader.
-	
-  	cout << tal << ' ';
-  	cin >> tal;
-  	cout << tal << ' ';
-  	cin >> tal;
-  	cout << tal << ' ';
-  	cin >> tal;
-  	cout << tal << ' ';
-  	cin >> tal;
-
-  	cout << tal << '\n' << endl;
+    //#svar# Tyckte det blev tydligare, men var felaktigt
+  	cout << integer << ' ';
+  	cin >> integer;
+  	cout << integer << ' ';
+  	cin >> integer;
+  	cout << integer << ' ';
+  	cin >> integer;
+  	cout << integer << ' ';
+  	cin >> integer;
+  	cout << integer << '\n' << endl;
 
   	cin.clear();
   	cin.ignore(1000, '\n');
 
   	//Tredje delen
   	cout << "Skriv in ett heltal och ett flyttal: " << flush;
-  	cin >> tal >> flyttal;
+  	cin >> integer >> floatnumber;
   	cout << "Du skrev in heltalet: " << flush;
-  	cout << setw(10) << tal << endl;
+  	cout << setw(10) << integer << endl;
   	cout << "Du skrev in flyttalet: " << flush;
-  	cout << setw(9) << fixed << setprecision(4) << flyttal << '\n' << endl;
+  	cout << setw(9) << fixed << setprecision(4) << floatnumber << '\n' << endl;
 
-	  cin.clear();
-	  cin.ignore(1000, '\n');
+	cin.clear();
+	cin.ignore(1000, '\n');
 
-	  //Fjärde delen
-	  //####
-	  //1-3 Använd konsekvent indenteringsstil.
-	  cout << "Skriv in ett flyttal och ett heltal: " << flush;
-	  cin >> flyttal >> tal;
-	  cout << "Du skrev in heltalet:" << flush;
-	  cout << setw(11) << setfill('-') << tal << endl;
+	//Fjärde delen
+	//####
+	//1-3 Använd konsekvent indenteringsstil.
+    //#svar# Tror det blev fel pga textredigeringsprogrammet,
+    //har ändrat inställningarna så det borde vara rätt.
+    cout << "Skriv in ett flyttal och ett heltal: " << flush;
+	cin >> floatnumber >> integer;
+	cout << "Du skrev in heltalet:" << flush;
+    cout << setw(11) << setfill('-') << integer << endl;
   	cout << "Du skrev in flyttalet:" << flush;
 	//####
-        //4-3 Upprepa inte manipulatorer som gäller tills vidare.
-  	cout << setw(10) << setfill('-') << fixed << setprecision(4)
-	  																 << flyttal << '\n' <<  endl;
+    //4-3 Upprepa inte manipulatorer som gäller tills vidare.
+    //#svar#Tog bort extra << setfill('-') << fixed << setprecision(4)
+  	cout << setw(10)  << floatnumber << '\n' <<  endl;
 	//####
-        //1-4 Radbryt långa rader på lämpligaste sätt.
+    //1-4 Radbryt långa rader på lämpligaste sätt.
+    //#svar# oklart vad lämpligast är, men tror de är rätt
 
   	cin.clear();
   	cin.ignore(1000, '\n');
 
-	  //Femte delen
-	  cout << "Skriv in ett tecken: " << flush;
-  	cin >> tecken;
-  	cout << "Du skrev in tecknet: " << tecken << '\n' << endl;
-
+	//Femte delen
+	cout << "Skriv in ett tecken: " << flush;
+  	cin >> character;
+  	cout << "Du skrev in tecknet: " << character << '\n' << endl;
 
     cin.clear();
     cin.ignore(1000, '\n');
@@ -91,8 +97,8 @@ int main()
 
     //Sjunde delen
     cout << "Skriv in ett heltal och ett ord: " << flush;
-    cin >> tal >> text;
-    cout << "Du skrev in talet |" << tal << "| och ordet |"
+    cin >> integer >> text;
+    cout << "Du skrev in talet |" << integer << "| och ordet |"
                                   << text << "|." << '\n' <<  endl;
 
     cin.clear();
@@ -100,9 +106,9 @@ int main()
 
     //Åttonde delen
     cout << "Skriv in ett tecken och ett ord: " << flush;
-    cin >> tecken >> text;
+    cin >> character >> text;
     cout << "Du skrev in \"" << text << "\" och \'"
-                             << tecken << "\'." << '\n' << endl;
+                             << character << "\'." << '\n' << endl;
     cin.clear();
     cin.ignore(1000, '\n');
 
