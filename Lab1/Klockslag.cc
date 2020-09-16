7ae7e43f9f33405e30c268676d3b25041acddb0f
Alfred <alfjo519@student.liu.se>
2020-09-16 14:17:28 +0200

    Test get_time

--- Lab1/Klockslag.cc
+++ Lab1/Klockslag.cc
@@ -9,17 +9,29 @@ Klockslag::Klockslag(int sec, int min, int h)
 
 }
 
+// int Klockslag::operator+(Time const & stuff)
+// {
+//     return
+// }
 
-bool    Klockslag::is_valid()
-        {
-            bool is_true{true};
+bool Klockslag::is_valid()
+{
+    bool is_true{true};
 
-            if (   0 >= hours   || hours >= 23
-                || 0 >= minutes || minutes >= 59
-                || !(0<=seconds<=59))
-            {
-                is_true = false;
-            }
+    if (   0 >= hours   || hours >= 23
+        || 0 >= minutes || minutes >= 59
+        || !(0<=seconds<=59))
+    {
+        is_true = false;
+    }
 
-            return is_true;
-        }
+    return is_true;
+}
+
+std::string Klockslag::get_time();
+{
+    std::string result = std::to_string(hours)
+                 + ':' + std::to_string(minutes)
+                 + ':' + std::to_string(seconds);
+    return result;
+}
